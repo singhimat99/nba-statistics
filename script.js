0463ee8eceead4898a25205ed3cf381786fe5791
Himat Singh <singhimat99@gmail.com>
2022-08-24 16:29:17 -0700

    finally got fetch to work, now need to style and write script

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const SEASON_AVERAGE_BASE_URL =
-  "https://www.balldontlie.io/api/v1/season_averages?player_ids[]=1,2,3,4,5,6&season=2021";
-const PLAYERS_BASE_URL = "https://www.balldontlie.io/api/v1/players";
+// import axios from "axios";
 
+const SEASON_AVERAGE_BASE_URL =
+  "https://www.balldontlie.io/api/v1/season_averages";
+const PLAYERS_BASE_URL = "https://www.balldontlie.io/api/v1/players";
+const TOTAL_PLAYERS = 3758;
 // Write your code here.
 const listOfPlayers = document.querySelector(".list-of-players");
 const selectedStat = document.getElementById("stats");
@@ -15,16 +17,30 @@ function displayList() {
   }
 }
 
-async function getPlayers() {
-  // const url = new URL(SEASON_AVERAGE_BASE_URL);
-  // url.searchParams.set("season", "2019");
-  // url.searchParams.set("players[]", "1,2,3,4");
+async function getPlayerStats() {
+  const url = new URL(SEASON_AVERAGE_BASE_URL);
+  const player_ids = Array.from(Array(1200).keys());
+  url.searchParams.set("player_ids[]", player_ids.join(","));
+  url.searchParams.set("season", "2021");
   try {
-    const response = await fetch(SEASON_AVERAGE_BASE_URL);
-    const obj = await response.json();
-    console.log(obj);
+    const response = await fetch(url);
+    const playerStats = await response.json();
+    return playerStats;
   } catch (error) {
     console.error("oh no: " + error);
   }
 }
-getPlayers();
+
+async function getAllPlayers(page) {
+  const playersUrl = new URL(PLAYERS_BASE_URL);
+  playersUrl.searchParams.set("page", `${page}`);
+  try {
+    const response = await fetch(playersUrl);
+    const players = await response.json();
+    return players;
+  } catch (error) {
+    console.log("oops" + error);
+  }
+}
+
+const playerStats = await getPlayerStats();
