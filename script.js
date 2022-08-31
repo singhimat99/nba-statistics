415e4e4bfc523c86fa1d8fb5eeb9be21aac49027
Himat Singh <singhimat99@gmail.com>
2022-08-30 20:54:36 -0700

    added validation and started the appendStats function

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-// import axios from "axios";
-
 const SEASON_AVERAGE_BASE_URL =
   "https://www.balldontlie.io/api/v1/season_averages";
 const ALL_PLAYERS_BASE_URL = "https://www.balldontlie.io/api/v1/players";
 // const SPECIFIC_PLAYER_BASE_URL = "https://www.balldontlie.io/api/v1/players/";
 const TOTAL_PLAYERS = 3758;
 // Write your code here.
-const listOfPlayers = document.querySelector(".list-of-players");
-const selectedStat = document.getElementById("stats");
+const playerNameInput = document.getElementById("stats");
+const getStatsButton = document.querySelector(".getStats");
 
-selectedStat.addEventListener("change", () => {
-  console.log(selectedStat.value);
-});
-function displayList() {
-  if (listOfPlayers.hasChildNodes() === true) {
-    listOfPlayers.style.display = "flex";
-  }
-}
+getStatsButton.addEventListener("click", appendStats);
 
 async function getPlayerStats(playerID, season = 2021) {
   const url = new URL(SEASON_AVERAGE_BASE_URL);
@@ -25,13 +16,14 @@ async function getPlayerStats(playerID, season = 2021) {
   try {
     const response = await fetch(url);
     const playerStats = await response.json();
-    return playerStats;
+    return playerStats.data[0]; // normal is an object with a data array
   } catch (error) {
     console.error("oh no: " + error);
   }
 }
 
-async function getPlayersByNameAndID(playerName) {
+// returns array of objects with that name plsu their IDs
+async function getPlayersByName(playerName) {
   const playersUrl = new URL(ALL_PLAYERS_BASE_URL);
   playersUrl.searchParams.set("search", `${playerName}`);
   try {
@@ -49,7 +41,32 @@ async function getPlayersByNameAndID(playerName) {
   }
 }
 
-const playersByNameAndID = await getPlayersByNameAndID("curry");
-console.log(playersByNameAndID);
-const playerStats = await getPlayerStats(playersByNameAndID[6].id);
-console.log(playerStats.data[0]["pts"]);
+async function validateInput(input) {
+  const playersByName = await getPlayersByName(input);
+  if (playersByName.length < 1) return false;
+  const players = playersByName.filter((name) => {
+    if (name.name.toLowerCase() === input) return name.name;
+  });
+  if (players.length < 1) return false;
+  return true;
+}
+// console.log(await validateInput("curry"));
+// const playerStats = await getPlayerStats(playersByName[6].id);
+// console.log(playerStats.data[0]["pts"]);
+
+async function appendStats() {
+  // clear any stats already appended if true
+
+  // disable button
+
+  // validate input
+  const input = playerNameInput.value;
+  if ((await validateInput(input)) === false) return;
+
+  // get stats of player requested
+  const player = await getPlayersByName(input);
+  const { name: playerName, id: playerID } = player[0];
+  const playerStats = await getPlayerStats(playerID);
+
+  //append those stats to a fragement and then to the tr
+}
